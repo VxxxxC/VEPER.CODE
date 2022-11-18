c09bfc19af1c2c087b1d448402dd028de19d0353
Veper(Mac) <veper.chesley@protonmail.com>
2022-11-18 19:01:24 +0800

    feat(init-astro-prettier): *

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [sitemap(), react()]
+  integrations: [sitemap(), react()],
 });
