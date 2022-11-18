b4cbe86b1f4bd71a0caa728f21d718905bc2502d
Veper(Mac) <veper.chesley@protonmail.com>
2022-11-18 19:44:36 +0800

    feat(add-tailwind-&-google-font): *

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [sitemap(), react()],
+  integrations: [sitemap(), react(), tailwind()]
 });
