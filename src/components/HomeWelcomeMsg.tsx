c289e5c595146d39014becd3168da977d4babb0d
Veper(Mac) <veper.chesley@protonmail.com>
2022-11-26 01:24:47 +0800

    fix(fix-tsc---noEmit-bugs): *

--- src/components/HomeWelcomeMsg.tsx
+++ src/components/HomeWelcomeMsg.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
-let text = 'Hi there 👋 Thank you to visit VEPER.CODE 🧑🏻‍💻 To more about me, please have a look at the below 🥳 ';
+let text =
+  "Hi there 👋 Thank you to visit VEPER.CODE 🧑🏻‍💻 To more about me, please have a look at the below 🥳 ";
 let i = 0;
 
 function HomeWelcomeMsg() {
-   if(i < text.length){
-    document.getElementById("welMsg").innerHTML += text.charAt(i);
+  if (i < text.length) {
+    const insert = document.getElementById("welMsg");
+    insert!.innerHTML += text.charAt(i);
     i++;
 
     setTimeout(HomeWelcomeMsg, 100);
   }
 }
 
-export default HomeWelcomeMsg
+export default HomeWelcomeMsg;
